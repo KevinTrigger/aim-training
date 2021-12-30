d49ea5e5df4e323cfec7b77a0ce102c304810532
KevinTrigger <80025182+KevinTrigger@users.noreply.github.com>
2021-12-30 11:11:46 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,114 @@
+function testWebP(callback) {
+  var webP = new Image();
+  webP.onload = webP.onerror = function () {
+    callback(webP.height == 2);
+  };
+  webP.src =
+    "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+testWebP(function (support) {
+  if (support == true) {
+    document.querySelector("body").classList.add("webp");
+  } else {
+    document.querySelector("body").classList.add("no-webp");
+  }
+});
+
+// ---------------------------------------------------------------------------
+
+const startButton = document.querySelector('#start');
+const screens = document.querySelectorAll('.screen');
+const timeList = document.querySelector("#time-list");
+const timeEl = document.querySelector('#time');
+const board = document.querySelector('#board');
+let time = 0;
+let score = 0;
+const colorsOfCircle = [
+   'yellow', 
+   'orange', 
+   'purple', 
+   'blue', 
+   'red', 
+   'brown', 
+   'pink', 
+   '#fff', 
+   '#f1f2f3'
+];
+
+startButton.addEventListener('click', event => {
+  event.preventDefault(); 
+  screens[0].classList.add('up')
+})
+
+timeList.addEventListener('click', event => {
+   if (event.target.classList.contains('time-btn')) {
+      time = parseInt(event.target.getAttribute('data-time'));
+      screens[1].classList.add('up');
+      startGame();
+   }
+})
+
+board.addEventListener('click', event => {
+   if (event.target.classList.contains('circle')) {
+      score++;
+      event.target.remove();
+      createRandomCircle();
+   }
+})
+
+function startGame() {
+   setInterval(decreaseTime, 1000);
+   createRandomCircle();
+   setTime(time);
+}
+
+function decreaseTime() {
+   if (time === 0) {
+      finishGame();
+   }
+   else {
+      let current = --time;
+      if (current < 10) {
+         current = `0${current}`;
+      }
+      setTime(current);
+   }
+   
+}
+
+function setTime(value) {
+   timeEl.innerHTML = `00:${value}`;
+}
+
+function finishGame() {
+   timeEl.parentNode.classList.add('hide');
+   board.innerHTML = `<h1>Cчёт: <span class="primary">${score}</span></h1>`;
+}
+
+function createRandomCircle() {
+   const circle = document.createElement('div');
+   const size = getRandomNumber(10, 60);
+   const {width, height} = board.getBoundingClientRect();
+   const x = getRandomNumber(0, width - size);
+   const y = getRandomNumber(0, height - size);
+
+   circle.classList.add('circle');
+   circle.style.backgroundColor = `${getRandomColor()}`;
+   circle.style.width = `${size}px`;
+   circle.style.height = `${size}px`;
+   circle.style.top = `${y}px`;
+   circle.style.left = `${x}px`;
+   board.append(circle);
+}
+
+function getRandomColor() {
+   const index = Math.floor(Math.random() * colorsOfCircle.length);
+   return colorsOfCircle[index];
+}
+
+function getRandomNumber(min, max) {
+   return Math.round(Math.random() * (max-min) + min);
+}
+
+
+
